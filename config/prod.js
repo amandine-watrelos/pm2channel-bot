73e30d211f9115c26bd9c892be0de8395babb715
Amandine Watrelos <amandine.watrelos@gmail.com>
2020-11-27 19:04:02 +0100

    chore (var) : bot token externalization

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    BOT_TOKEN: process.env.BOT_TOKEN,
+};
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+const config = require("./config/keys.js");
 
 const client = new Discord.Client();
 
